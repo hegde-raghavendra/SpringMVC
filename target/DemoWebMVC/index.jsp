e0c17dc536c74941d7bf8126dca39cac7e6bc5ff
ragha <ragha@10.0.1.38>
2019-02-19 22:59:37 -0600

    work in progress

--- target/DemoWebMVC/index.jsp
+++ target/DemoWebMVC/index.jsp
@@ -1,5 +1,11 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+
+<form action="add">
+  First name: <input type="text" name="t1"><br>
+  Last name:  <input type="text" name="t2"><br>
+  <input type="submit" value="Submit">
+</form>
+
 </body>
 </html>
---
+++ target/classes/com/rgh/AddController.java
@@ -0,0 +1,15 @@
+package com.rgh;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class AddController {
+	
+	@RequestMapping("/add")
+	public void add() {
+		System.out.println("I am here");
+	}
+
+}
